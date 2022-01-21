e059d492f53de1a20bfd0f4c424ca0724f3166eb
Caitlin Wall <caitlinwall96@gmail.com>
2022-01-21 17:57:20 -0500

    Add html routes

---
+++ routes/html/html-routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+
+// GET Route for homepage 
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/assets/pages/notes.html'))
+);
+
+module.exports = app;
