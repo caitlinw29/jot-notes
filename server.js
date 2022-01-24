18c7d7bef1bb5a7585a807f0cd0a827794a6ded9
Caitlin Wall <caitlinwall96@gmail.com>
2022-01-23 23:08:29 -0500

    Add html routes and require path

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 
@@ -8,7 +9,17 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use('/route', routes);
+app.use('/api', routes);
+
+// GET Route for homepage 
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/assets/pages/notes.html'))
+);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
