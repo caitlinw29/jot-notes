99373182b8bcef815bec44da819f67c0b9053f01
Caitlin Wall <caitlinwall96@gmail.com>
2022-01-24 00:11:55 -0500

    Adjust html route for index.html to be *

--- server.js
+++ server.js
@@ -11,14 +11,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use('/api', routes);
 
-// GET Route for homepage 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
 // GET Route for notes page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// GET Route for homepage 
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.listen(PORT, () =>
